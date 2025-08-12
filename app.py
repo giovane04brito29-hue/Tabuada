4b947318e4f7c9586a910aa63de6185abfdeb0d0
giovane04brito29-hue <giovane04brito29@gmail.com>
2025-08-12 11:05:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-n = int(input("Tabuada de: "))
-inicio = int(input("De: "))
-fim = int(input("Até: "))
+n = int(input("Tabuada de: ")) #aqui você coloca o número que você deseja multiplicar
+inicio = int(input("De: ")) #Aqui da onde ele começa
+fim = int(input("Até: "))  #até onde termina obs: pode ser qualquer número
 x = inicio
 while x <= fim:
     print(f"{n} x {x} = {n * x}")
